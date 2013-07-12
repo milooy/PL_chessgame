38cb751492a5b2fce754658508acff1232caa469
jkc1352000 <jkc1352000@nhnnext.org>
2013-07-12 15:39:10 +0900

    연습문제 1-4

---
+++ src/Pawn.java
@@ -0,0 +1,9 @@
+
+public class Pawn {
+	String color;
+	
+	Pawn(){
+		color = "white";
+	}
+
+}
--- test/PawnTest.java
+++ test/PawnTest.java
@@ -4,5 +4,7 @@ import junit.framework.TestCase;
 public class PawnTest extends TestCase {
 	public void testCreate() throws Exception {
 		Pawn myPawn = new Pawn();
+		String pawnColor = myPawn.color;
+		assertEquals("white", pawnColor);
 	}
 }
