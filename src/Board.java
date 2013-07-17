d59547d54a88b44fe9b2cc272ee77cd33d9cbbfe
jkc1352000 <jkc1352000@nhnnext.org>
2013-07-17 18:53:19 +0900

    연습문제 2-4

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,19 @@
+import java.util.ArrayList;
 
 public class Board {
+	private int NumOfPawn = 0;
+	private ArrayList<Pawn> PawnList= new ArrayList<Pawn>();
 	
+	public void addPawn(Pawn addPawn){
+		PawnList.add(addPawn);
+		NumOfPawn++;
+	}
+	
+	public int getNumOfPawn(){
+		return NumOfPawn;
+	}
+	
+	public boolean hasPawn(Pawn testPawn){
+		return PawnList.contains(testPawn);
+	}
 }
--- src/Pawn.java
+++ src/Pawn.java
@@ -13,4 +13,29 @@ public class Pawn {
 		this.color = color;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((color == null) ? 0 : color.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pawn other = (Pawn) obj;
+		if (color == null) {
+			if (other.color != null)
+				return false;
+		} else if (!color.equals(other.color))
+			return false;
+		return true;
+	}
+
 }
