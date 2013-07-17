96be2618a567189f5e19fbcc115b09baf038c88b
jkc1352000 <jkc1352000@nhnnext.org>
2013-07-17 21:22:56 +0900

    연습문제 2-6

---
+++ src/chess/BoardTest.java
@@ -0,0 +1,20 @@
+package chess;
+import junit.framework.TestCase;
+
+
+public class BoardTest extends TestCase {
+	private static final String WHITE = "white";
+	private static final String BLACK = "black";
+	
+	Board myBoard = new Board();
+	
+	public void testaddPawn() throws Exception {
+		myBoard.addPawn(new Pawn(BLACK));
+		myBoard.addPawn(new Pawn(WHITE));
+		
+		assertEquals(2, myBoard.getNumOfPawn());
+		assertEquals(true, myBoard.hasPawn(new Pawn(BLACK)));
+	}
+	
+
+}
