25b82f441030902ecb4187a4e7027470f90dd84a
jkc1352000 <jkc1352000@nhnnext.org>
2013-07-19 16:19:00 +0900

    연습문제 3-2

--- test/CharacterTest.java
+++ test/CharacterTest.java
@@ -1,10 +1,15 @@
 import junit.framework.TestCase;
 
-
 public class CharacterTest extends TestCase {
 	public void testWhitespace() throws Exception {
 		assertTrue(Character.isWhitespace('\n'));
 		assertTrue(Character.isWhitespace('\t'));
 		assertTrue(Character.isWhitespace(' '));
+	}
+	
+	public void testIdentifier() throws Exception {
+		assertEquals(false, Character.isJavaIdentifierPart('^'));  // 인자가 Identifier 규칙에 맞는지 검사
+		assertEquals(true, Character.isJavaIdentifierPart('_'));
+		assertEquals(true, Character.isJavaIdentifierPart('2'));
 	}
 }
